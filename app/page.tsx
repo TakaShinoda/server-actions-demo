8239cf0c1c1445844fa8eada8014e84206681e70
Takahiro <tkhr_621@docomo.ne.jp>
2023-11-04 00:01:11 +0900

    task を表示する table ui を作成

--- app/page.tsx
+++ app/page.tsx
@@ -39,6 +39,65 @@ export default function Home() {
           Add
         </button>
       </form>
+
+      <div className="w-1/4">
+        <div className="mt-8">
+          <div className="overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead className="bg-white/5">
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-white sm:pl-6"
+                  >
+                    Id
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                  >
+                    Task
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-white"
+                  >
+                    Done
+                  </th>
+                  <th scope="col" className="relative py-3.5 pl-3 pr-4 sm:pr-6">
+                    <span className="sr-only">Edit</span>
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white/5">
+                <tr>
+                  <td className="py-4 pl-4 pr-3 text-sm font-medium text-white sm:pl-6">
+                    1
+                  </td>
+                  <td className="px-3 py-4 text-sm text-white">
+                    あいうえおあいうえおあいうえおあいうえお
+                  </td>
+                  <td className="px-3 py-4 text-sm text-white">
+                    <input
+                      type="checkbox"
+                      className="h-4 w-4 rounded border-gray-300 text-sky-600 focus:ring-sky-600"
+                    />
+                  </td>
+                  <td className="relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6">
+                    <button
+                      type="button"
+                      className="text-red-600 hover:text-red-900"
+                    >
+                      Delete
+                    </button>
+                  </td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+
     </main>
   )
 }

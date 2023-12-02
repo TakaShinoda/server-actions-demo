d103d189f18431b5d41c97969f5b6ff017a412f2
Takahiro <tkhr_621@docomo.ne.jp>
2023-12-03 01:02:54 +0900

    削除ボタンコンポーネント作成

---
+++ app/_components/DeleteButton.tsx
@@ -0,0 +1,22 @@
+'use client'
+import { useTransition } from 'react'
+import { deleteTask } from '../actions'
+import type { Task } from '../types'
+
+type Props = {
+  task: Task
+}
+
+export const DeleteButton = ({ task }: Props) => {
+  console.log(task)
+  const [isPending, startTransition] = useTransition()
+  return (
+    <button
+      onClick={() => startTransition(() => deleteTask(task.id))}
+      type="button"
+      className="text-red-600 hover:text-red-900"
+    >
+      {isPending ? 'loading...' : 'Delete'}
+    </button>
+  )
+}
